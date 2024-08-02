99d17d7dbe826ca697752c5131989e25c61a6e84
Cooper <12110517@mail.sustech.edu.cn>
2024-08-02 16:09:20 +0800

    fix path name

--- BadNet/data_download.py
+++ BadNet/data_download.py
@@ -2,7 +2,7 @@ import os
 import urllib.request
 from util import download_data
 
-data_path = './datasetsss/'
+data_path = './dataset/'
 os.makedirs(data_path, exist_ok=True)
 download_data('mnist', True, data_path)
 download_data('cifar', True, data_path)
